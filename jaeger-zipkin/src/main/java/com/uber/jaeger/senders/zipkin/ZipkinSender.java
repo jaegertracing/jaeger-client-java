b35c0e16fdf4f15fad0c2142bae3b098b527039d
Luke Amdor <luke.amdor@gmail.com>
2016-10-13 16:42:16 -0500

    zipkin: backfill annotations with host when sending

--- jaeger-zipkin/src/main/java/com/uber/jaeger/senders/zipkin/ZipkinSender.java
+++ jaeger-zipkin/src/main/java/com/uber/jaeger/senders/zipkin/ZipkinSender.java
@@ -21,7 +21,7 @@
  */
 package com.uber.jaeger.senders.zipkin;
 
-import com.twitter.zipkin.thriftjava.Span;
+import com.twitter.zipkin.thriftjava.*;
 import com.uber.jaeger.exceptions.SenderException;
 import com.uber.jaeger.senders.Sender;
 
@@ -94,7 +94,7 @@ public final class ZipkinSender implements Sender {
    */
   @Override
   public int append(Span span) throws SenderException {
-    byte[] next = encoder.encode(span);
+    byte[] next = encoder.encode(backFillHostOnAnnotations(span));
     int messageSizeOfNextSpan = delegate.messageSizeInBytes(Collections.singletonList(next));
     // don't enqueue something larger than we can drain
     if (Integer.compare(messageSizeOfNextSpan, delegate.messageMaxBytes()) > 0) {
@@ -160,5 +160,32 @@ public final class ZipkinSender implements Sender {
       throw new SenderException("Failed to close " + delegate, e, n);
     }
     return n;
+  }
+
+  // serviceName/host is needed on annotations so zipkin can query
+  // see https://github.com/uber/jaeger-client-java/pull/75 for more info
+  Span backFillHostOnAnnotations(Span span) {
+    Endpoint host = null;
+    for (BinaryAnnotation binaryAnnotation : span.getBinary_annotations()) {
+      if (zipkincoreConstants.LOCAL_COMPONENT.equals(binaryAnnotation.getKey())) {
+        host = binaryAnnotation.getHost();
+      }
+    }
+
+    if (host != null) {
+      for (BinaryAnnotation binaryAnnotation : span.getBinary_annotations()) {
+        if (!binaryAnnotation.isSetHost()) {
+          binaryAnnotation.setHost(host);
+        }
+      }
+
+      for (Annotation annotation : span.getAnnotations()) {
+        if (!annotation.isSetHost()) {
+          annotation.setHost(host);
+        }
+      }
+    }
+
+    return span;
   }
 }
