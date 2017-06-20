a4ce2b046082dcfd462119a49314ed2ce008ffa9
Marcin Biegan <marcin.biegan@getbase.com>
2017-06-20 16:37:46 +0200

    Fix conversion of ip address to int (#199)

--- jaeger-core/src/test/java/com/uber/jaeger/UtilsTest.java
+++ jaeger-core/src/test/java/com/uber/jaeger/UtilsTest.java
@@ -22,7 +22,9 @@
 
 package com.uber.jaeger;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 import com.uber.jaeger.exceptions.EmptyIpException;
 import com.uber.jaeger.exceptions.NotFourOctetsException;
@@ -49,9 +51,22 @@ public class UtilsTest {
   }
 
   @Test
-  public void testIpToInt32() {
-    int expectedIp = (127 << 24) | 1;
-    int actualIp = Utils.ipToInt("127.0.0.1");
-    assertEquals(expectedIp, actualIp);
+  public void testIpToInt32_localhost() {
+    assertThat(Utils.ipToInt("127.0.0.1"), equalTo((127 << 24) | 1));
+  }
+
+  @Test
+  public void testIpToInt32_above127() {
+    assertThat(Utils.ipToInt("10.137.1.2"), equalTo((10 << 24) | (137 << 16) | (1 << 8) | 2));
+  }
+
+  @Test
+  public void testIpToInt32_zeros() {
+    assertThat(Utils.ipToInt("0.0.0.0"), equalTo(0));
+  }
+
+  @Test
+  public void testIpToInt32_broadcast() {
+    assertThat(Utils.ipToInt("255.255.255.255"), equalTo(-1));
   }
 }
