51c7ea78c443d5cc0545d6db2f2dc3590400ce6b
Gary Brown <gary@brownuk.com>
2017-10-02 09:30:36 +0100

    Remove the deprecated log methods
    Signed-off-by: Gary Brown <gary@brownuk.com>

--- jaeger-core/src/main/java/com/uber/jaeger/Span.java
+++ jaeger-core/src/main/java/com/uber/jaeger/Span.java
@@ -236,30 +236,21 @@ public class Span implements io.opentracing.Span {
 
   @Override
   public Span log(String event) {
-    return log(tracer.clock().currentTimeMicros(), event, null);
+    return log(tracer.clock().currentTimeMicros(), event);
   }
 
   @Override
   public Span log(long timestampMicroseconds, String event) {
-    return log(timestampMicroseconds, event, null);
-  }
-
-  @Deprecated
-  public Span log(String message, /* @Nullable */ Object payload) {
-    return log(tracer.clock().currentTimeMicros(), message, payload);
-  }
-
-  @Deprecated
-  public Span log(long timestampMicroseconds, String message, /* @Nullable */ Object payload) {
     synchronized (this) {
-      if (message == null && payload == null) {
+      if (event == null) {
         return this;
       }
       if (context.isSampled()) {
         if (logs == null) {
           this.logs = new ArrayList<LogData>();
         }
-        logs.add(new LogData(timestampMicroseconds, message, payload));
+        // TODO: Consider removing payload from LogData
+        logs.add(new LogData(timestampMicroseconds, event, null));
       }
       return this;
     }
