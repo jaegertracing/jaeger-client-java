e1caa77ecc251f09be7923b341f84edc3026f875
Debosmit Ray <debo@uber.com>
2018-01-20 15:25:57 -0800

    [jaeger-client-java/jax-rs2] Remove jaeger-context dependence for JAX-RS2 tracing instrumentation
    Signed-off-by: Debosmit Ray <debo@uber.com>

--- jaeger-jaxrs2/src/test/java/com/uber/jaeger/filters/jaxrs2/ClientFilterTest.java
+++ jaeger-jaxrs2/src/test/java/com/uber/jaeger/filters/jaxrs2/ClientFilterTest.java
@@ -60,7 +60,7 @@ public class ClientFilterTest {
     tracer =
         new com.uber.jaeger.Tracer.Builder("Angry Machine", reporter, new ConstSampler(true))
             .build();
-    undertest = new ClientFilter(tracer, null);
+    undertest = new ClientFilter(tracer);
   }
 
   @Test
