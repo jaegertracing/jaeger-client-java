2afb4140fbb0d197148e7ba4daed01d7d507e044
Pavol Loffay <ploffay@redhat.com>
2018-04-11 13:35:41 +0200

    Use travis stages to simplify config
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- travis/build-crossdock.sh
+++ travis/build-crossdock.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-make crossdock-fresh
-
 export REPO=jaegertracing/xdock-java
 export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 export TAG=`if [ "$BRANCH" == "master" ]; then echo "latest"; else echo "${BRANCH///}"; fi`
