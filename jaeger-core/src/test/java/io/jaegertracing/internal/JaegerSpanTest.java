48112ed842703822e6c579306cdfe52c7e9adf83
Isaac Hier <ihier@uber.com>
2018-08-20 16:32:28 -0400

    Specify expected string output with string formatting
    Signed-off-by: Isaac Hier <ihier@uber.com>

--- jaeger-core/src/test/java/io/jaegertracing/internal/JaegerSpanTest.java
+++ jaeger-core/src/test/java/io/jaegertracing/internal/JaegerSpanTest.java
@@ -211,7 +211,12 @@ public class JaegerSpanTest {
   public void testSpanToString() {
     String operation = "test-operation";
     JaegerSpan span = tracer.buildSpan(operation).start();
-    String expectedString = span.context().toString() + " - " + operation;
+    String expectedString = String.format("%x:%x:%x:%x - %s",
+        span.context().getTraceId(),
+        span.context().getSpanId(),
+        span.context().getParentId(),
+        span.context().getFlags(),
+        operation);
     assertEquals(expectedString, span.toString());
     span.finish();
   }
