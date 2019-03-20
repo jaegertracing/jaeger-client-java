17b1e7b497700292c3b7a15a0740a07c2333ba42
Iori Yoneji <yoneji_i@worksap.co.jp>
2019-03-20 23:46:59 +0900

    add note for pre-generated, for-crossdock certificate and fix its conf
    Signed-off-by: Iori Yoneji <yoneji_i@worksap.co.jp>

--- jaeger-crossdock/https-proxy/gen.sh
+++ jaeger-crossdock/https-proxy/gen.sh
@@ -9,6 +9,25 @@ fi
 
 SERVER=$1
 FORWARD=$2
+
+# Authority.crt & authority.key is pre-generated with hash code of sha256/n6Ovey/sJws9vKJpESmWyQf9Oocak9J51mmPKGm4S0E=
+# About this file:
+#   CN:        TEST CA
+#   Algorithm: ECDSA-SHA256
+#   Validity:
+#     Not Before: Mar 7 14:38:20 2019 GMT
+#     Not After:  Mar 7 14:38:20 2029 GMT
+#
+# To generate key: (not needed for cert renewal)
+# openssl ecparam -genkey -name prime256v1 -out authority.key
+#
+# To regenerate:
+# openssl req -new -sha256 -key authority.key -out authority.csr -subj "/CN=TEST CA/"
+# openssl x509 -trustout -signkey authority.key -days 3652 -req -in authority.csr -out authority.crt
+#
+# To check pin:
+# openssl ec -in authority.key -outform der -pubout | openssl dgst -sha256 -binary | openssl enc -base64
+AUTHORITY=authority
 TARGET=build/proxy
 
 # generate secrets
@@ -17,10 +36,10 @@ mkdir -p ./build
 openssl ecparam -genkey -name prime256v1 -out ${TARGET}.key
 openssl req -new -sha256 -key ${TARGET}.key -out ${TARGET}.csr -subj \
     "/CN=${SERVER}/"
-openssl x509 -req -sha256 -days 1 -CA authority.crt -CAkey authority.key -CAcreateserial -in ${TARGET}.csr -out ${TARGET}.crt
+openssl x509 -req -sha256 -days 1 -CA ${AUTHORITY}.crt -CAkey ${AUTHORITY}.key -CAcreateserial -in ${TARGET}.csr -out ${TARGET}.crt
 chmod 644 ${TARGET}.key
 
-cat authority.crt >> ${TARGET}.crt
+cat ${AUTHORITY}.crt >> ${TARGET}.crt
 
 # generate nginx settings
 SERVER=${SERVER} FORWARD=${FORWARD} envsubst < "proxy.template.conf" > ${TARGET}.conf
