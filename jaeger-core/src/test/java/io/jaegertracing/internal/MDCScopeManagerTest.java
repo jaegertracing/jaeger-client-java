39257544b0c54f2f2314ecf6a9d711d552180894
avim <avi.maslati@forescout.com>
2020-06-26 18:01:31 +0300

    fix style errors
    Signed-off-by: avim <avi.maslati@forescout.com>

--- jaeger-core/src/test/java/io/jaegertracing/internal/MDCScopeManagerTest.java
+++ jaeger-core/src/test/java/io/jaegertracing/internal/MDCScopeManagerTest.java
@@ -14,6 +14,9 @@
 
 package io.jaegertracing.internal;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 import io.jaegertracing.internal.reporters.InMemoryReporter;
 import io.jaegertracing.internal.samplers.ConstSampler;
 import io.opentracing.Scope;
@@ -24,9 +27,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.slf4j.MDC;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 public class MDCScopeManagerTest {
 
@@ -78,8 +78,8 @@ public class MDCScopeManagerTest {
 
   @Test
   public void testCustomKeysCreation() {
-    ScopeManager mdcScopeManager = new MDCScopeManager.
-            Builder()
+    ScopeManager mdcScopeManager = new MDCScopeManager
+            .Builder()
             .withMDCTraceIdKey("CustomTraceId")
             .withMDCSampledKey("customSampled")
             .withMDCSpanIdKey("customSpanId")
@@ -98,8 +98,8 @@ public class MDCScopeManagerTest {
 
   @Test
   public void testCustomAndDefaultKeysCreation() {
-    ScopeManager mdcScopeManager = new MDCScopeManager.
-            Builder()
+    ScopeManager mdcScopeManager = new MDCScopeManager
+            .Builder()
             .withMDCSampledKey("customSampled")
             .withMDCSpanIdKey("customSpanId")
             .build();
@@ -123,7 +123,11 @@ public class MDCScopeManagerTest {
             .build();
   }
 
-  private void assertSpanContextEqualsToMDC(JaegerSpanContext context, String traceIDKey, String spanIdKey, String sampledKey) {
+  private void assertSpanContextEqualsToMDC(JaegerSpanContext context,
+                                            String traceIDKey,
+                                            String spanIdKey,
+                                            String sampledKey) {
+
     assertEquals(context.toTraceId(), MDC.get(traceIDKey));
     assertEquals(context.toSpanId(), MDC.get(spanIdKey));
     assertEquals(String.valueOf(context.isSampled()), MDC.get(sampledKey));
