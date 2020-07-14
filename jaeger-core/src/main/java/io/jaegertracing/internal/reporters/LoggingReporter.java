58ae1c66c8056e1b66776a2f751fdd238bcb2f48
daming <daming@foxmail.com>
2020-07-14 15:11:55 +0800

    avoid holding too many span instances in memory
    Signed-off-by: daming <daming@foxmail.com>

--- jaeger-core/src/main/java/io/jaegertracing/internal/reporters/LoggingReporter.java
+++ jaeger-core/src/main/java/io/jaegertracing/internal/reporters/LoggingReporter.java
@@ -40,7 +40,7 @@ public class LoggingReporter implements Reporter {
 
   @Override
   public void report(JaegerSpan span) {
-    logger.info("Span reported: {}", span);
+    logger.info("Span reported: {}", span.toString());
   }
 
   @Override
